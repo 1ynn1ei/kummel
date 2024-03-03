ceff74e4e443c7d7e433b5d4b3ab98291ae97ec5
Lynn Leichtle <brooke.leichtle@gmail.com>
2024-03-03 22:27:19 +0100

    adding clap for better ergonomics

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,38 @@ mod stream;
 mod lexer;
 use std::fs;
 use lexer::Lexer;
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    path: String,
+    #[arg(short, long)]
+    output: Option<String>
+}
 
 fn main() {
-    let data = fs::read("./sample.js").unwrap();
-    let mut lexer = Lexer::new(&data);
-    loop {
-        let token = lexer.next_token();
-        if let Ok(Some(token)) = token {
-            println!("{:?}", token);
-            match token.token {
-                definition::Token::EndOfFile => {
-                    return;
-                },
-                _ => { continue }
+    let cli = Cli::parse();
+    let file = cli.path;
+    match fs::read(file) {
+        Ok(data) => {
+            let mut lexer = Lexer::new(&data);
+            loop {
+                let token = lexer.next_token();
+                if let Ok(Some(token)) = token {
+                    println!("{:?}", token);
+                    match token.token {
+                        definition::Token::EndOfFile => {
+                            return;
+                        },
+                        _ => { continue }
+                    }
+                }
             }
+        },
+        Err(e) => {
+            println!("{}", e);
         }
     }
 }
